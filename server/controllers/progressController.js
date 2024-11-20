05e49ef6b7ab0eb6b33f46ba1c03657bc9be4676
andre <dr.dre212@gmail.com>
2024-11-20 16:49:48 -0500

    "Added changes for debugging

--- server/controllers/progressController.js
+++ server/controllers/progressController.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const fetchAllMetricsProgress = async (membershipType, membershipId, accessToken) => {
     try {
       // Fetch all metrics progress in one request
