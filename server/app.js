210cbfcb266757d34785ffc44c318e9995f31faa
andre <dr.dre212@gmail.com>
2024-11-20 03:42:35 -0500

    Added environment variable logging for debugging

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const URL = process.env.URL;
 const connectDB = async () => {
     try {
       // Connect to MongoDB
-      await mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log('MongoDB connected');
     } catch (error) {
       console.error('MongoDB connection error:', error.message);
