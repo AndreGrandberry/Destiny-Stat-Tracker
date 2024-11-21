820805f2d8e0145c2ba788a9a34c152e1856bec4
andre <dr.dre212@gmail.com>
2024-11-20 22:09:22 -0500

    "Changes for debugging"

--- server/app.js
+++ server/app.js
@@ -69,7 +69,7 @@ app.use(
 
 // app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('dist')); // Serve the frontend files 
+app.use(express.static(path.join(__dirname, 'dist'))); 
 
 //Routes
 app.use("/auth", auth);
