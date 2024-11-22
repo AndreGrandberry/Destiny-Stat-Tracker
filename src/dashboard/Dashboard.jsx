20cfe9bb3db99ca176f04a78222330bd3b630f6a
andre <dr.dre212@gmail.com>
2024-11-22 03:48:39 -0500

    "Debug"

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
   const handleCategoryClick = (categoryName) => {
     setSelectedCategory(categoryName);
     window.scrollTo({
-      top: document.getElementById('metrics-section').offsetTop - 50, 
+      top: document.getElementById('metrics-section2').offsetTop - 50, 
       left: 0,
       behavior: 'smooth', 
     });
@@ -68,7 +68,7 @@ const Dashboard = () => {
       </div>
 
       {/* Render metrics for the selected category */}
-      <div id="metrics-section"className="metrics">
+      <div id="metrics-section2"className="metrics">
         {selectedCategory &&
           metricsData
             .filter((category) => category.categoryName === selectedCategory)
